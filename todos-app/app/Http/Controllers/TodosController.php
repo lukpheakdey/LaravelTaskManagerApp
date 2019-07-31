b8847278c364efa19cec97935c5a265771edb1f3
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-07-30 21:40:07 -0400

    Laravel
    - Laravel Todo List Management Application

---
+++ todos-app/app/Http/Controllers/TodosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodosController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('todos.index')->with('todos', $todos);
+    }
+}
